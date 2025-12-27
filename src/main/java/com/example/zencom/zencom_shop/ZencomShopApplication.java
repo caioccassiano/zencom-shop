828e0ca2158d8f82f8b2947680ad796fbb06e564
caioccassiano <caioccassiano@icloud.com>
2025-12-27 13:39:27 +0100

    initial commit

---
+++ src/main/java/com/example/zencom/zencom_shop/ZencomShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.zencom.zencom_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZencomShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZencomShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zencom/zencom_shop/ZencomShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zencom.zencom_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZencomShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
